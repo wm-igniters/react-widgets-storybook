88de0fa92e2b39a8cf89cf1961728e28e4ad695d
rajkumarreddyabbadi <rajkumar.abbadi@wavemaker.com>
2025-12-16 10:05:23 +0530

    fixes

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,1 +1,44 @@
+import { addons } from "storybook/manager-api";
+import { create } from "storybook/theming/create";
 import "../style/theme.css";
+
+const customTheme = create({
+  base: "light", // or 'dark'
+  brandUrl: "https://wavemaker.com",
+  brandImage: "./Logo.png", // Replace with your logo URL
+  brandTarget: "_blank", // Open brandUrl in a new tab
+
+  fontBase: "neurial-grotesk-regular",
+
+  // Colors
+  // colorPrimary: "rgba(135, 135, 135, 1)",
+  colorSecondary: "#296df6",
+
+  // UI
+  appBg:
+    "linear-gradient(170deg, rgb(226, 244, 254) 14.38%, rgb(218, 217, 252) 99.86%)",
+  appContentBg: "#ffffff",
+  appPreviewBg: "#ffffff",
+  // appBorderColor: "rgba(0, 0, 0, 0.10)",
+  appBorderRadius: 6,
+
+  // Text colors
+  textColor: "rgba(48, 48, 48, 1)",
+  textInverseColor: "#ffffff",
+
+  // Toolbar default and active colors
+  // barTextColor: "#5E686F",
+  // barSelectedColor: "rgba(41, 109, 246, 0.14)",
+  // barHoverColor: "#5E686F",
+  // barBg: "#fff",
+
+  // Form colors
+  // inputBg: "#ffffff",
+  // inputBorder: "rgba(0, 0, 0, 0.10)",
+  // inputTextColor: "rgba(48, 48, 48, 1)",
+  // inputBorderRadius: 4,
+});
+
+addons.setConfig({
+  theme: customTheme,
+});
